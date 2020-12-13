3099ed892cb85b9d44e6424acfd4a569d29c04d7
zulqarnain-99 <zulqi99@gmail.com>
2020-12-13 21:07:03 +0500

    add sql data source

--- WebForm1.aspx
+++ WebForm1.aspx
@@ -8,7 +8,20 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div style="font-family:Arial">
+            <asp:Button ID="btnGetDataFromDB" runat="server" Text="Get Data From Database" OnClick="btnGetDataFromDB_Click" />
+            <asp:GridView ID="gvStudents" runat="server" AutoGenerateColumns="False" DataKeyNames="ID" OnRowCancelingEdit="gvStudents_RowCancelingEdit" OnRowDeleting="gvStudents_RowDeleting" OnRowEditing="gvStudents_RowEditing" OnRowUpdating="gvStudents_RowUpdating">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                    <asp:BoundField DataField="ID" HeaderText="ID" InsertVisible="False" ReadOnly="True" SortExpression="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                    <asp:BoundField DataField="TotalMarks" HeaderText="TotalMarks" SortExpression="TotalMarks" />
+                </Columns>
+            </asp:GridView>
+             <asp:Button ID="btnUpdateDB" runat="server" Text="Update Database Table" />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            
         </div>
     </form>
 </body>
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -4,12 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Disconnected_data_access_ASP.Net
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetDataFromDB()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            string strSelectQuery = "select * from tblStudents";
+            SqlDataAdapter da = new SqlDataAdapter(strSelectQuery, con);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Students");
+
+            ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["ID"] };
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(24), System.Web.Caching.Cache.NoSlidingExpiration);
+            gvStudents.DataSource = ds;
+            gvStudents.DataBind();
+
+            lblMessage.Text = "Data Loaded from Database";
+        }
+        private void GetDataFromCache()
+        {
+            if (Cache["DATASET"] != null)
+            {
+                gvStudents.DataSource = (DataSet)Cache["DATASET"];
+                gvStudents.DataBind();
+            }
+        }
+        
+
+        protected void btnGetDataFromDB_Click(object sender, EventArgs e)
+        {
+            GetDataFromDB();
+        }
+
+        protected void gvStudents_RowEditing(object sender, GridViewEditEventArgs e)
+        { // Set row in editing mode
+            gvStudents.EditIndex = e.NewEditIndex;
+            GetDataFromCache();
+        }
+
+        protected void gvStudents_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void gvStudents_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvStudents.EditIndex = -1;
+            GetDataFromCache();
+        }
+
+        protected void gvStudents_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
 
         }
